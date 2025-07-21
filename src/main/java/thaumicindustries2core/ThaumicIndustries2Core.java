473d5c5a4d95d253b57280373332bcbec560f0cc
Thenemex <tnmx777@gmail.com>
2025-07-21 07:37:40 +0200

    Set version to 1.1

--- src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
+++ src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
@@ -7,22 +7,22 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import nemexlib.api.util.Logger;
 import nemexlib.config.AConfig;
 import thaumicindustries2core.config.Config;
-import thaumicindustries2core.model.config.ConfigRecipes;
+import thaumicindustries2core.model.config.ConfigExpertTweaks;
 
 import static thaumicindustries2core.ThaumicIndustries2Core.dependencies;
 import static thaumicindustries2core.ThaumicIndustries2Core.modID;
 
 @SuppressWarnings({"unused", "EmptyMethod"})
-@Mod(modid = modID, useMetadata = true, version = "1.0", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.1", dependencies = dependencies)
 public class ThaumicIndustries2Core{
 
-    public static final String modID = "TCI2Core", modName = "ThaumicIndustries2Core", configVersion = "1.0";
+    public static final String modID = "TCI2Core", modName = "ThaumicIndustries2Core";
     public static AConfig config;
     public static final Logger logger = new Logger(modID);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        config = new Config(event, configVersion).init(); // Init config // Custom folder with mod name
+        config = new Config(event, "1.1").init(); // Init config // Custom folder with mod name
     }
 
     @Mod.EventHandler
@@ -32,7 +32,7 @@ public class ThaumicIndustries2Core{
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent ignoredEvent) {
         // Loading recipe changes
-        ConfigRecipes.init();
+        ConfigExpertTweaks.init();
     }
 
     public static final String dependencies = "required-after:Thaumcraft@[4.2.3.5,);required-after:NemexLib@[1.0.0.3,)";
