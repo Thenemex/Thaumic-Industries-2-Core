8779ac4f5fc301671572c0e393e2bec959029be4
Thenemex <tnmx777@gmail.com>
2025-07-21 07:38:00 +0200

    Added config entry for expert vanilla furnace compound recipe

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -7,7 +7,7 @@ import static thaumicindustries2core.ThaumicIndustries2Core.modName;
 
 public class Config extends AConfig {
 
-    public static boolean scribingTools;
+    public static boolean scribingTools, vanillaFurnace;
 
     public Config(FMLPreInitializationEvent event, String version) {
         super(event, modName, modName, version);
@@ -17,5 +17,6 @@ public class Config extends AConfig {
         String expert = "Expert-Tweaks";
         config.addCustomCategoryComment(expert, "You can tweak off/on expert changes here");
         scribingTools = newEntry(expert, "ScribingTools", true);
+        vanillaFurnace = newEntry(expert, "VanillaFurnace", true);
     }
 }
