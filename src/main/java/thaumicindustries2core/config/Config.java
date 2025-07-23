f906703ce62cbbc2e758ff43080a176a913ed2cc
Thenemex <tnmx777@gmail.com>
2025-07-23 11:26:18 +0200

    Adding comments for config

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -8,15 +8,16 @@ import static thaumicindustries2core.ThaumicIndustries2Core.modName;
 public class Config extends AConfig {
 
     public static boolean scribingTools, vanillaFurnace;
+    protected static String version = "1.1.1";
 
-    public Config(FMLPreInitializationEvent event, String version) {
-        super(event, modName, modName, version);
+    public Config(FMLPreInitializationEvent event) {
+        super(event, modName, modName, "1.1.1");
     }
 
     protected void loadConfig() {
         String expert = "Expert-Tweaks";
         config.addCustomCategoryComment(expert, "You can tweak off/on expert changes here");
-        scribingTools = newEntry(expert, "ScribingTools", true);
-        vanillaFurnace = newEntry(expert, "VanillaFurnace", true);
+        scribingTools = newEntry(expert, "ScribingTools", true, "Sets a new arcane recipe, that will only accept TC4 Phials and not empty bottles");
+        vanillaFurnace = newEntry(expert, "VanillaFurnace", true, "Sets a new mystical construct recipe for the vanilla furnace, and adds another one with arcane stone");
     }
 }
