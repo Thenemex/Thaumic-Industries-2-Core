5bb9e88d6ab5183e6c0a5c84212dca216d80d87a
Thenemex <tnmx777@gmail.com>
2025-07-23 11:04:13 +0200

    Added rest of code for the wand handler

--- src/main/java/thaumicindustries2core/model/events/VanillaFurnaceHandler.java
+++ src/main/java/thaumicindustries2core/model/events/VanillaFurnaceHandler.java
@@ -9,7 +9,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import thaumcraft.common.items.wands.ItemWandCasting;
 
-import static thaumicindustries2core.ThaumicIndustries2Core.logger;
+import static thaumicindustries2core.model.research.VanillaFurnaceCompoundRecipes.compound;
 
 public class VanillaFurnaceHandler extends WandEventHandler {
 
@@ -42,14 +42,12 @@ public class VanillaFurnaceHandler extends WandEventHandler {
         ItemWandCasting wand = (ItemWandCasting) heldItem.getItem();
         if (wand.getFocus(heldItem) != null) return false; // Needs no focus equipped on the wand
         if (!player.isSneaking()) return false; // Player needs to be sneaking
-        // Code for editing world
-        // if (canFitVanillaFurnace(world, x, y, z) && wand.consumeAllVisCrafting(heldItem, player, VanillaFurnaceCompoundRecipes.compound, true))
-        logger.info("FINAL = ", canFitVanillaFurnace(world, x, y, z));
+        if (isMatchingBlueprint(world, x, y, z) && wand.consumeAllVisCrafting(heldItem, player, compound, true))
+            return replaceStructure(world, x, y, z, side); // Delete all blocks and place the furnace at y-1
         return false;
     }
 
-    protected boolean canFitVanillaFurnace(World world, int x, int y, int z) {
-        logger.info("Clicked at : (" + x + "," + y + "," + z + ")");
+    protected boolean isMatchingBlueprint(World world, int x, int y, int z) {
         Block block;
         boolean fit;
         for (int yy = 0; yy < 3; yy++)
@@ -60,11 +58,20 @@ public class VanillaFurnaceHandler extends WandEventHandler {
                     if (fit && block.equals(slab))
                         // If block matches blueprint AND is a slab -> check for metadata 3 (Cobblestone Slab)
                         fit = world.getBlockMetadata(x + xx - 1, y - yy + 1, z + zz - 1) == 3;
-                    logger.info(block, " -> ", fit);
                     if (!fit) return false;
                 }
         return true;
     }
 
-
+    protected boolean replaceStructure(World world, int x, int y, int z, int side) {
+        for (int yy = 0; yy < 3; yy++)
+            for (int xx = 0; xx < 3; xx++)
+                for (int zz = 0; zz < 3; zz++)
+                    world.setBlockToAir(x + xx - 1, y - yy + 1, z + zz - 1);
+        world.setBlock(x, y - 1, z, Blocks.furnace);
+        world.setBlockMetadataWithNotify(x, y - 1, z, side,3);
+        world.playSoundEffect((double)x + 0.5, (double)y + 0.5, (double)z + 0.5,
+                "thaumcraft:wand", 1.0F, 1.0F);
+        return true;
+    }
 }
