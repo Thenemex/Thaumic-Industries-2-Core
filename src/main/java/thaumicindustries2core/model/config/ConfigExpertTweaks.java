79af45e18e8628f08f525ee5ed22cbeb1ec406f0
Thenemex <tnmx777@gmail.com>
2025-08-08 05:09:53 +0200

    Changed Thaumometer recipe to arcane, with a bit of vis.

--- src/main/java/thaumicindustries2core/model/config/ConfigExpertTweaks.java
+++ src/main/java/thaumicindustries2core/model/config/ConfigExpertTweaks.java
@@ -2,10 +2,12 @@ package thaumicindustries2core.model.config;
 
 import nemexlib.api.events.WandEventHandler;
 import nemexlib.api.recipes.arcane.ArcaneAdder;
+import nemexlib.api.recipes.workbench.WorkbenchRemover;
 import nemexlib.api.thaumcraft.API;
 import nemexlib.api.thaumcraft.aspects.Aspects;
 import nemexlib.api.thaumcraft.research.AResearch;
 import nemexlib.api.thaumcraft.research.Research;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -13,7 +15,9 @@ import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.crafting.IArcaneRecipe;
 import thaumcraft.api.research.ResearchItem;
 import thaumcraft.api.research.ResearchPage;
+import thaumcraft.common.config.ConfigItems;
 import thaumcraft.common.config.ConfigResearch;
+import thaumicindustries2core.ThaumicIndustries2Core;
 import thaumicindustries2core.model.events.VanillaFurnaceHandler;
 import thaumicindustries2core.model.research.VanillaFurnaceCompoundRecipes;
 
@@ -21,10 +25,8 @@ import java.util.HashMap;
 
 import static net.minecraftforge.oredict.OreDictionary.getOres;
 import static thaumcraft.api.aspects.Aspect.*;
-import static thaumcraft.common.config.ConfigItems.itemEssence;
-import static thaumcraft.common.config.ConfigItems.itemInkwell;
-import static thaumicindustries2core.config.Config.scribingTools;
-import static thaumicindustries2core.config.Config.vanillaFurnace;
+import static thaumcraft.common.config.ConfigItems.*;
+import static thaumicindustries2core.config.Config.*;
 
 public class ConfigExpertTweaks {
 
@@ -33,6 +35,7 @@ public class ConfigExpertTweaks {
 
     public static void init() {
         if (scribingTools) loadExpertScribingTools_ARCANE();
+        if (thaumometer) loadExpertThaumometer_ARCANE();
         if (vanillaFurnace) loadExpertVanillaFurnace_COMPOUND();
     }
 
@@ -49,9 +52,32 @@ public class ConfigExpertTweaks {
         // Adding the page with Arcane Recipe
         IArcaneRecipe[] recipes = new IArcaneRecipe[getOres("dyeBlack").size()];
         for (int i = 0; i < getOres("dyeBlack").size(); i++)
-            recipes[i] = ArcaneAdder.addArcane("RESEARCH", new Aspects(new Aspect[]{ORDER, AIR, WATER}, 5, 3, 3), true, false, new ItemStack(itemInkwell),
-                    new ItemStack(itemEssence, 1, 0), new ItemStack(Items.feather), getOres("dyeBlack").get(i));
+            recipes[i] = ArcaneAdder.addArcane("RESEARCH", new Aspects(new Aspect[]{ORDER, AIR, WATER}, 5, 3, 3),
+                    true, false, new ItemStack(itemInkwell),
+                    new ItemStack(itemEssence, 1, 0),
+                    new ItemStack(Items.feather),
+                    getOres("dyeBlack").get(i)); // ToDo try to implement ore recipe for lighter NEI
         API.addPage(research, new ResearchPage(recipes), 6);
+    }
+
+    // Remove recipes from CraftingManager AND ConfigRecipes TC4
+    // Fish OreDict Like-> new ItemStack(Items.field_151115_aP, 1, 32767)
+    protected static void loadExpertThaumometer_ARCANE() {
+        ResearchItem research = API.getResearch("ARTIFICE", "THAUMOMETER");
+        // Removing recipe pages for research
+        API.removePage(research, 2);
+        // Removing recipes for Thaumometer
+        ItemStack thaumometer = new ItemStack(itemThaumometer);
+        WorkbenchRemover.i().removeItem(thaumometer);
+        ConfigResearch.recipes.remove("Thaumometer");
+        // Adding the page with Arcane recipe
+        IArcaneRecipe recipe = ArcaneAdder.addArcane("THAUMOMETER", new Aspects(new Aspect[]{AIR, EARTH, ORDER}, 3, 8, 5),
+                false, false, thaumometer,
+                " S ", "GVG", " S ",
+                'S', new ItemStack(itemShard, 1, 32767),
+                'G', new ItemStack(Items.gold_ingot),
+                'V', new ItemStack(Blocks.glass));
+        API.addPage(research, new ResearchPage(recipe), 2);
     }
 
     protected static void loadExpertVanillaFurnace_COMPOUND() {
