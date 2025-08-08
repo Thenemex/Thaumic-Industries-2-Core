34e47fc93ea014a41458abf39971e71b6c1c9e01
Thenemex <tnmx777@gmail.com>
2025-08-08 04:57:08 +0200

    Changed required dependency version of NemexLib to 1007

--- src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
+++ src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
@@ -35,5 +35,5 @@ public class ThaumicIndustries2Core{
         ConfigExpertTweaks.init();
     }
 
-    public static final String dependencies = "required-after:Thaumcraft@[4.2.3.5,);required-after:NemexLib@[1.0.0.3,)";
+    public static final String dependencies = "required-after:Thaumcraft@[4.2.3.5,);required-after:NemexLib@[1.0.0.7,)";
 }
