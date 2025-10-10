ecc2a271497f04e6e83dd5c3becb00b0292464ff
Thenemex <tnmx777@gmail.com>
2025-10-10 14:39:32 +0200

    Adding new expert recipe for Bone Bow

--- src/main/java/thaumicindustries2core/model/config/ConfigExpertTweaks.java
+++ src/main/java/thaumicindustries2core/model/config/ConfigExpertTweaks.java
@@ -1,6 +1,7 @@
 package thaumicindustries2core.model.config;
 
 import nemexlib.api.events.WandEventHandler;
+import nemexlib.api.items.ItemFinder;
 import nemexlib.api.recipes.arcane.ArcaneAdder;
 import nemexlib.api.recipes.workbench.WorkbenchRemover;
 import nemexlib.api.thaumcraft.API;
@@ -14,12 +15,14 @@ import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.crafting.IArcaneRecipe;
 import thaumcraft.api.research.ResearchItem;
 import thaumcraft.api.research.ResearchPage;
+import thaumcraft.common.config.ConfigItems;
 import thaumcraft.common.config.ConfigResearch;
 import thaumicindustries2core.model.events.VanillaFurnaceHandler;
 import thaumicindustries2core.model.research.VanillaFurnaceCompoundRecipes;
 
 import java.util.HashMap;
 
+import static nemexlib.api.items.ItemFinder.findItemTC;
 import static net.minecraftforge.oredict.OreDictionary.getOres;
 import static thaumcraft.api.aspects.Aspect.*;
 import static thaumcraft.common.config.ConfigItems.*;
@@ -30,10 +33,13 @@ public class ConfigExpertTweaks {
     public static WandEventHandler vanillaFurnaceHandler;
     public final static HashMap<String, AResearch> researchMap = new HashMap<>();
 
+    public static ItemStack specialString = new ItemStack(Items.string);
+
     public static void init() {
         if (scribingTools) loadExpertScribingTools_ARCANE();
         if (thaumometer) loadExpertThaumometer_ARCANE();
         if (vanillaFurnace) loadExpertVanillaFurnace_COMPOUND();
+        if (boneBow) loadExpertBoneBow_ARCANE();
     }
 
     protected static void loadExpertScribingTools_ARCANE() {
@@ -46,7 +52,7 @@ public class ConfigExpertTweaks {
         ConfigResearch.recipes.remove("Scribe1");
         CraftingManager.getInstance().getRecipeList().remove(ConfigResearch.recipes.get("Scribe2"));
         ConfigResearch.recipes.remove("Scribe2");
-        // Adding the page with Arcane Recipe
+        // Adding the page with Arcane recipe
         IArcaneRecipe[] recipes = new IArcaneRecipe[getOres("dyeBlack").size()];
         for (int i = 0; i < getOres("dyeBlack").size(); i++)
             recipes[i] = ArcaneAdder.addArcane("RESEARCH", new Aspects(new Aspect[]{ORDER, AIR, WATER}, 5, 3, 3),
@@ -56,7 +62,6 @@ public class ConfigExpertTweaks {
                     getOres("dyeBlack").get(i)); // ToDo try to implement ore recipe for lighter NEI
         API.addPage(research, new ResearchPage(recipes), 6);
     }
-
     protected static void loadExpertThaumometer_ARCANE() {
         ResearchItem research = API.getResearch("ARTIFICE", "THAUMOMETER");
         // Removing recipe pages for research
@@ -69,7 +74,7 @@ public class ConfigExpertTweaks {
         WorkbenchRemover.i().removeItem(thaumometer);
         ConfigResearch.recipes.remove("Thaumometer");
         // Adding the page with Arcane recipe
-        IArcaneRecipe[] recipes = new IArcaneRecipe[2];
+        IArcaneRecipe[] recipes = new IArcaneRecipe[2]; // ToDo Add boolean config for rotated recipe
         for (int i = 0; i < recipes.length; i++)
                 recipes[i] = ArcaneAdder.addArcane("THAUMOMETER", new Aspects(new Aspect[]{AIR, EARTH, ORDER}, 3, 8, 5),
                 false, false, thaumometer,
@@ -79,11 +84,27 @@ public class ConfigExpertTweaks {
                 'V', new ItemStack(Blocks.glass));
         API.addPage(research, new ResearchPage(recipes), 2);
     }
-
     protected static void loadExpertVanillaFurnace_COMPOUND() {
         VanillaFurnaceHandler.initBlueprint();
         vanillaFurnaceHandler = new VanillaFurnaceHandler();
         // Loading the research
         researchMap.put(VanillaFurnaceCompoundRecipes.tag, new VanillaFurnaceCompoundRecipes());
+    }
+
+    protected static void loadExpertBoneBow_ARCANE() {
+        ResearchItem research = API.getResearch("ARTIFICE", "BONEBOW");
+        // Removing recipe for BoneBow
+        CraftingManager.getInstance().getRecipeList().remove(ConfigResearch.recipes.get("BoneBow"));
+        ConfigResearch.recipes.remove("BoneBow");
+        // Replacing the page with Arcane recipe
+        IArcaneRecipe recipe = ArcaneAdder.addArcane("BONEBOW", new Aspects(new Aspect[]{ENTROPY, AIR}, 60, 35),
+                false, false, new ItemStack(itemBowBone),
+                "SB ", "YWR", "SB ",
+                'S', new ItemStack(Items.string),
+                'B', new ItemStack(Items.bone),
+                'Y', specialString, // Will turn to Yarn if Witching Gagdets is loaded
+                'W', new ItemStack(Items.bow),
+                'R', findItemTC("WandRod", 7)); // Bone Rod
+        API.replacePage(research, new ResearchPage(recipe), 2);
     }
 }
