082d06794ddafce602b271cd9a2001e3c327a2a0
Thenemex <tnmx777@gmail.com>
2025-10-11 22:55:42 +0200

    Adding new config entries

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -7,9 +7,9 @@ import static thaumicindustries2core.ThaumicIndustries2Core.modName;
 
 public class Config extends AConfig {
 
-    public static boolean scribingTools, thaumometer, vanillaFurnace, boneBow;
-    public static boolean wgEnabled;
-    protected static String version = "1.3";
+    public static boolean scribingTools, thaumometer, vanillaFurnace, boneBow, crystalWell;
+    public static boolean fmEnabled, wgEnabled;
+    protected static String version = "1.4";
 
     public Config(FMLPreInitializationEvent event) {
         super(event, modName, modName, version);
@@ -22,8 +22,10 @@ public class Config extends AConfig {
         thaumometer = newEntry(expert, "Thaumometer", true, "Sets a new arcane recipe using some vis");
         vanillaFurnace = newEntry(expert, "VanillaFurnace", true, "Adds a mystical construct recipe for the vanilla furnace, and adds another one with arcane stone");
         boneBow = newEntry(expert, "BoneBow", true, "Sets a new arcane recipe with harder components");
+        crystalWell = newEntry(expert, "CrystalScribingTools", true, "Sets a new infusion recipe with harder components");
 
         config.addCustomCategoryComment(integrations, "You can turn off/on integrations here");
+        fmEnabled = newEntry(integrations, "Forbidden Magic");
         wgEnabled = newEntry(integrations, "Witching Gadgets");
     }
 }
