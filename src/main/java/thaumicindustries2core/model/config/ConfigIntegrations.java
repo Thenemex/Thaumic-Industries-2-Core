8ec451b7a3be4e031a6f053a6bc56de12de35a0b
Thenemex <tnmx777@gmail.com>
2025-10-12 15:04:34 +0200

    Loading compat module in proper class

--- src/main/java/thaumicindustries2core/model/config/ConfigIntegrations.java
+++ src/main/java/thaumicindustries2core/model/config/ConfigIntegrations.java
@@ -2,15 +2,18 @@ package thaumicindustries2core.model.config;
 
 import cpw.mods.fml.common.Loader;
 import thaumicindustries2core.config.Config;
+import thaumicindustries2core.model.integrations.ForbiddenMagicCompat;
 import thaumicindustries2core.model.integrations.WitchingGadgetsCompat;
 
 public class ConfigIntegrations {
 
-    public static final String wg = "WitchingGadgets";
+    public static final String fm = "ForbiddenMagic", wg = "WitchingGadgets";
 
     public static void init() {
         if (modIsLoaded(wg, Config.wgEnabled))
             new WitchingGadgetsCompat(wg);
+        if (modIsLoaded(fm, Config.fmEnabled))
+            new ForbiddenMagicCompat(fm);
     }
 
     public static boolean modIsLoaded(String mod, boolean config) {
