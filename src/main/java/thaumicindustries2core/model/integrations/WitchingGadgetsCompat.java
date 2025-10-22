a2b2becea8580ac5a2a1d0e6c02cf4eb0c2e0a15
Thenemex <tnmx777@gmail.com>
2025-10-22 15:51:08 +0200

    Set expert recipe for Seraph's Shoulders [WG]

--- src/main/java/thaumicindustries2core/model/integrations/WitchingGadgetsCompat.java
+++ src/main/java/thaumicindustries2core/model/integrations/WitchingGadgetsCompat.java
@@ -1,8 +1,22 @@
 package thaumicindustries2core.model.integrations;
 
 import nemexlib.api.integrations.ACompat;
-import nemexlib.api.items.ItemFinder;
+import nemexlib.api.recipes.infusion.InfusionAdder;
+import nemexlib.api.recipes.infusion.InfusionRemover;
+import nemexlib.api.thaumcraft.API;
+import nemexlib.api.thaumcraft.aspects.Aspects;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.crafting.InfusionRecipe;
+import thaumcraft.api.research.ResearchItem;
+import thaumcraft.api.research.ResearchPage;
 import thaumicindustries2core.model.config.ConfigExpertTweaks;
+import witchinggadgets.common.WGResearch;
+
+import static nemexlib.api.items.ItemFinder.findItem;
+import static nemexlib.api.items.ItemFinder.findItemTC;
+import static thaumcraft.api.aspects.Aspect.*;
 
 public class WitchingGadgetsCompat extends ACompat {
 
@@ -13,9 +27,36 @@ public class WitchingGadgetsCompat extends ACompat {
     @Override
     public void loadIntegration() {
         addYarnToExpertBoneBow();
+        loadExpertSeraphShoulders_INFUSION(); // ToDo Config entry
     }
 
     protected void addYarnToExpertBoneBow() {
-        ConfigExpertTweaks.specialString = ItemFinder.findItem(mod, "item.WG_Material", 0);
+        ConfigExpertTweaks.specialString = findItem(mod, "item.WG_Material");
+    }
+
+    protected void loadExpertSeraphShoulders_INFUSION() {
+        ResearchItem research = API.getResearch("WITCHGADG", "WGBAUBLES");
+        // Adding Arcane Bellows as prereq
+        API.addParents(research, true, "BELLOWS");
+        // Naming items
+        ItemStack seraphShoulders = findItem(mod, "item.WG_Baubles"),
+                airShard = findItemTC("ItemShard"),
+                bellows = findItemTC("blockWoodenDevice"),
+                feather = new ItemStack(Items.feather);
+        // Removing recipe for Seraph Shoulders
+        new InfusionRemover(2, ThaumcraftApi.getCraftingRecipes(), WGResearch.recipeList.values()).removeItem(seraphShoulders);
+        // Replacing the page with new recipe
+        InfusionRecipe recipe = InfusionAdder.addInfusion("WGBAUBLES", 4,
+                new Aspects(new int[]{60, 40, 30, 20, 15}, FLIGHT, AIR, ORDER, MOTION, GREED),
+                seraphShoulders, findItem("TravellersGear", "simpleGear", 4),
+                airShard,
+                bellows,
+                feather,
+                bellows,
+                airShard,
+                bellows,
+                feather,
+                bellows);
+        API.replacePage(research, new ResearchPage(recipe), 8);
     }
 }
