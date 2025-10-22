41dfdacb2d6f7145bc0b909148ad10dc23a3b8ac
Thenemex <tnmx777@gmail.com>
2025-10-22 15:53:40 +0200

    Adding new config entry for Seraph's Shoulders

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -7,9 +7,9 @@ import static thaumicindustries2core.ThaumicIndustries2Core.modName;
 
 public class Config extends AConfig {
 
-    public static boolean scribingTools, thaumometer, vanillaFurnace, boneBow, crystalWell;
+    public static boolean scribingTools, thaumometer, vanillaFurnace, boneBow, crystalWell, seraphShoulders;
     public static boolean fmEnabled, wgEnabled;
-    protected static String version = "1.4";
+    protected static String version = "1.5";
 
     public Config(FMLPreInitializationEvent event) {
         super(event, modName, modName, version);
@@ -23,6 +23,7 @@ public class Config extends AConfig {
         vanillaFurnace = newEntry(expert, "VanillaFurnace", true, "Adds a mystical construct recipe for the vanilla furnace, and adds another one with arcane stone");
         boneBow = newEntry(expert, "BoneBow", true, "Sets a new arcane recipe with harder components");
         crystalWell = newEntry(expert, "CrystalScribingTools", true, "Sets a new infusion recipe with harder components");
+        seraphShoulders = newEntry(expert, "SeraphShoulders", true, "Sets a new infusion recipe with arcane bellows and more essentia");
 
         config.addCustomCategoryComment(integrations, "You can turn off/on integrations here");
         fmEnabled = newEntry(integrations, "Forbidden Magic");
