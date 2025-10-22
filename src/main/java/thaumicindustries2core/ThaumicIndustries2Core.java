77cf981e4c995372366ce9a0f220f381694703c3
Thenemex <tnmx777@gmail.com>
2025-10-22 15:59:26 +0200

    Set version to 1.5

--- src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
+++ src/main/java/thaumicindustries2core/ThaumicIndustries2Core.java
@@ -14,7 +14,7 @@ import static thaumicindustries2core.ThaumicIndustries2Core.dependencies;
 import static thaumicindustries2core.ThaumicIndustries2Core.modID;
 
 @SuppressWarnings({"unused", "EmptyMethod"})
-@Mod(modid = modID, useMetadata = true, version = "1.4.0.1", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.5", dependencies = dependencies)
 public class ThaumicIndustries2Core{
 
     public static final String modID = "TCI2Core", modName = "ThaumicIndustries2Core";
