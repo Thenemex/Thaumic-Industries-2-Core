082562d2b6fb51c7f319e0d71e861ed28f5b5a59
Thenemex <tnmx777@gmail.com>
2026-03-08 21:10:02 +0100

    Added new config entry for newest addition to come

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -8,7 +8,8 @@ import static thaumicindustries2core.ThaumicIndustries2Core.modName;
 public class Config extends AConfig {
 
     public static boolean scribingTools, thaumometer, vanillaFurnace, boneBow, crystalWell, seraphShoulders;
-    public static boolean fmEnabled, wgEnabled;
+    public static boolean woolToStringMerge;
+    public static boolean fmEnabled, mcEnabled, wgEnabled;
     protected static final String version = "1.5";
 
     public Config(FMLPreInitializationEvent event) {
@@ -16,17 +17,23 @@ public class Config extends AConfig {
     }
 
     protected void loadConfig() {
-        String expert = "Expert-Tweaks", integrations = "Mod-Integrations";
+        String expert = "Expert-Tweaks";
         config.addCustomCategoryComment(expert, "You can tweak off/on expert changes here");
-        scribingTools = newEntry(expert, "ScribingTools", true, "Sets a new arcane recipe, that will only accept TC4 Phials and not empty bottles");
-        thaumometer = newEntry(expert, "Thaumometer", true, "Sets a new arcane recipe using some vis");
-        vanillaFurnace = newEntry(expert, "VanillaFurnace", true, "Adds a mystical construct recipe for the vanilla furnace, and adds another one with arcane stone");
-        boneBow = newEntry(expert, "BoneBow", true, "Sets a new arcane recipe with harder components");
-        crystalWell = newEntry(expert, "CrystalScribingTools", true, "Sets a new infusion recipe with harder components");
-        seraphShoulders = newEntry(expert, "SeraphShoulders", true, "Sets a new infusion recipe with arcane bellows and more essentia");
+        scribingTools = newEntry(expert, "ScribingTools", "Sets a new arcane recipe, that will only accept TC4 Phials and not empty bottles");
+        thaumometer = newEntry(expert, "Thaumometer", "Sets a new arcane recipe using some vis");
+        vanillaFurnace = newEntry(expert, "VanillaFurnace", "Adds a mystical construct recipe for the vanilla furnace, and adds another one with arcane stone");
+        boneBow = newEntry(expert, "BoneBow", "Sets a new arcane recipe with harder components");
+        crystalWell = newEntry(expert, "CrystalScribingTools", "Sets a new infusion recipe with harder components");
+        seraphShoulders = newEntry(expert, "SeraphShoulders", "Sets a new infusion recipe with arcane bellows and more essentia");
 
+        String recipeMerges = "Recipe-Merges";
+        config.addCustomCategoryComment(recipeMerges, "You can tweak off/on recipe merges here");
+        woolToStringMerge = newEntry(recipeMerges, "Wool -> String", "Delete all 4 additionnal recipes, and add one under Alchemy / Entropic Process");
+
+        String integrations = "Mod-Integrations";
         config.addCustomCategoryComment(integrations, "You can turn off/on integrations here");
         fmEnabled = newEntry(integrations, "Forbidden Magic");
+        mcEnabled = newEntry(integrations, "Magic Cookies");
         wgEnabled = newEntry(integrations, "Witching Gadgets");
     }
 }
