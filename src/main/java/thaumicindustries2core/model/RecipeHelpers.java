f648caabd60e19740d009467d9d6c0f6349856bc
Thenemex <tnmx777@gmail.com>
2026-03-09 18:52:54 +0100

    Changed the way recipe helpers are instanciated

--- src/main/java/thaumicindustries2core/model/RecipeHelpers.java
+++ src/main/java/thaumicindustries2core/model/RecipeHelpers.java
@@ -4,12 +4,23 @@ import nemexlib.api.recipes.arcane.ArcaneRemover;
 import nemexlib.api.recipes.crucible.CrucibleRemover;
 import nemexlib.api.recipes.infusion.InfusionRemover;
 import nemexlib.api.recipes.workbench.WorkbenchRemover;
+import thaumcraft.api.ThaumcraftApi;
 
+import java.util.List;
+
+@SuppressWarnings("rawtypes")
 public class RecipeHelpers {
 
-    public static final WorkbenchRemover workbenchRemover = WorkbenchRemover.i();
-    public static final ArcaneRemover arcaneRemover = new ArcaneRemover();
-    public static final CrucibleRemover crucibleRemover = new CrucibleRemover();
-    public static final InfusionRemover infusionRemover = new InfusionRemover();
+    public static WorkbenchRemover workbenchRemover;
+    public static ArcaneRemover arcaneRemover;
+    public static CrucibleRemover crucibleRemover;
+    public static InfusionRemover infusionRemover;
 
+    public static void init() {
+        List tcRecipes = ThaumcraftApi.getCraftingRecipes();
+        workbenchRemover = WorkbenchRemover.i();
+        arcaneRemover = new ArcaneRemover(tcRecipes);
+        crucibleRemover = new CrucibleRemover(tcRecipes);
+        infusionRemover = new InfusionRemover(tcRecipes);
+    }
 }
