8a179c84c1d27e26a696bd56aedcc93b50349b17
Thenemex <tnmx777@gmail.com>
2026-07-14 16:55:41 +0200

    Set vis and research for newest handler

--- src/main/java/thaumicindustries2core/model/events/AlternativeLeatherHandler.java
+++ src/main/java/thaumicindustries2core/model/events/AlternativeLeatherHandler.java
@@ -6,13 +6,15 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import thaumcraft.common.config.ConfigBlocks;
 import thaumicindustries2core.config.Config;
+import thaumicindustries2core.model.research.AlternativeLeatherCompoundRecipe;
 
 public class AlternativeLeatherHandler extends SingleBlockWithDropsHandler {
 
+    // ToDo Check if possible to do something that give high amount for first time users, then 1
     public AlternativeLeatherHandler() {
         super(new BlockType(ConfigBlocks.blockTaint, 2));
-        // ToDo Set research tag
-        // ToDo Set Vis
+        this.researchTag = "ALTERNATIVELEATHER";
+        this.setVis(AlternativeLeatherCompoundRecipe.compound);
     }
 
     @Override
