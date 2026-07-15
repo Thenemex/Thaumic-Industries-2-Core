f9fb7d628e1a912add549e12c12ef4f0237e1b1c
Thenemex <tnmx777@gmail.com>
2026-07-15 17:10:11 +0200

    Patched a bug in the compound recipe from AlternativeLeatherHandler

--- src/main/java/thaumicindustries2core/model/events/AlternativeLeatherHandler.java
+++ src/main/java/thaumicindustries2core/model/events/AlternativeLeatherHandler.java
@@ -1,6 +1,6 @@
 package thaumicindustries2core.model.events;
 
-import nemexlib.api.events.SingleBlockWithDropsHandler;
+import nemexlib.api.events.SingleBlockWithDropsHandlerWithoutUpgrade;
 import nemexlib.api.items.types.BlockType;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -8,17 +8,16 @@ import thaumcraft.common.config.ConfigBlocks;
 import thaumicindustries2core.config.Config;
 import thaumicindustries2core.model.research.AlternativeLeatherCompoundRecipe;
 
-public class AlternativeLeatherHandler extends SingleBlockWithDropsHandler {
+public class AlternativeLeatherHandler extends SingleBlockWithDropsHandlerWithoutUpgrade {
 
     // ToDo Check if possible to do something that give high amount for first time users, then 1
     public AlternativeLeatherHandler() {
-        super(new BlockType(ConfigBlocks.blockTaint, 2));
-        this.researchTag = "ALTERNATIVELEATHER";
+        super("ALTERNATIVELEATHER", new BlockType(ConfigBlocks.blockTaint, 2));
         this.setVis(AlternativeLeatherCompoundRecipe.compound);
     }
 
     @Override
-    protected ItemStack getDrops(int i, boolean b) {
+    protected ItemStack getDrops() {
         return new ItemStack(Items.leather, Config.alternativeCompoundLeather_outputAmount);
     }
 }
