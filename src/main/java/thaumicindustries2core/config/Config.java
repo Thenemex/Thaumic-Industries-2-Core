4b1aed8cc39c9e2f4f61e04b7b317a8e1db75116
Thenemex <tnmx777@gmail.com>
2026-08-23 18:24:20 +0200

    Set mod version to 1.16

--- src/main/java/thaumicindustries2core/config/Config.java
+++ src/main/java/thaumicindustries2core/config/Config.java
@@ -15,7 +15,7 @@ public class Config extends AConfig {
     public static boolean removeDislocationFocus;
     public static boolean alternativeVisFilter, alternativeCompoundLeather;
     public static int alternativeVisFilter_outputAmount, alternativeCompoundLeather_outputAmount;
-    protected static final String version = "1.13";
+    protected static final String version = "1.16";
 
     public Config(FMLPreInitializationEvent event) {
         super(ThaumicIndustries2Core.modName, event, version);
